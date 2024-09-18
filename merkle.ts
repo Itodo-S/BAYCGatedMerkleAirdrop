8d6cf9cb2f85e9004112f33e495a65b37fa058a2
Itodo <itodosimonitodo1@gmail.com>
2024-09-18 19:13:44 +0100

    1 passed test

--- merkle.ts
+++ merkle.ts
@@ -1,60 +1,82 @@
-import { StandardMerkleTree } from "@openzeppelin/merkle-tree";
-import fs from "fs";
-import csv from "csv-parser";
+import { ethers } from "hardhat";
+import MerkleTree from "merkletreejs";
+import csvParser from "csv-parser";
+const fs = require("fs");
+import keccak256 from "keccak256";
 
-const csvFilePath = "./airdrop.csv"; 
-const searchAddress = "0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB"; 
-
-async function readCSV(filePath) {
+// Function to generate merkle root
+async function generateRootHash(): Promise<string> {
   return new Promise((resolve, reject) => {
-    const values = [];
-    fs.createReadStream(filePath)
-      .pipe(csv())
-      .on("data", (row) => {
-        values.push([row.address, row.amount]);
+    let results: Buffer[] = [];
+
+    fs.createReadStream('airdrop.csv')
+      .pipe(csvParser())
+      .on('data', (row: { address: string; amount: string }) => {  
+        const address = row.address;
+        const amount = row.amount;
+        const leaf = keccak256(
+          ethers.solidityPacked(["address", "uint256"], [address, amount])
+        );
+        results.push(leaf);
       })
-      .on("end", () => {
-        resolve(values);
+      .on('end', () => {
+        const tree = new MerkleTree(results, keccak256, {
+          sortPairs: true,
+        });
+
+        const roothash = tree.getHexRoot();
+        console.log('Merkle Root:', roothash);
+
+        resolve(roothash);  
       })
-      .on("error", reject);
+      .on('error', reject); 
   });
 }
 
-async function generateMerkleTree() {
-  try {
-    const values = await readCSV(csvFilePath);
-    const tree = StandardMerkleTree.of(values, ["address", "uint256"]);
-    console.log("Merkle Root:", tree.root);
-    fs.writeFileSync("tree.json", JSON.stringify(tree.dump()));
-  } catch (error) {
-    console.error("Error generating Merkle tree:", error);
-  }
+// The function then generates a proof for the target address and amount, and returns the proof.
+async function generateMerkleProof( targetAddress: string, targetAmount: string, ): Promise<string[]> {
+  const userData = await getUserDataFromCSV();
+  
+  return new Promise((resolve, reject) => {
+    console.log(`Starting to generate proof for address: ${targetAddress}, amount: ${targetAmount}`);
+
+    let results: Buffer[] = userData.map((user) => 
+      keccak256(
+        ethers.solidityPacked(["address", "uint256"], [user.address, user.amount])
+      )
+    );
+
+    const tree = new MerkleTree(results, keccak256, {
+      sortPairs: true,
+    });
+
+    const targetLeaf = keccak256(
+      ethers.solidityPacked(["address", "uint256"], [targetAddress, targetAmount])
+    );
+
+    const proof = tree.getHexProof(targetLeaf);
+    // console.log(proof);
+    
+    resolve(proof);
+  });
 }
 
-async function generateProof() {
-  const tree = StandardMerkleTree.load(
-    JSON.parse(fs.readFileSync("tree.json", "utf8"))
-  );
-  for (const [i, v] of tree.entries()) {
-    if (v[0] === searchAddress) {
-      const proof = tree.getProof(i);
-      console.log(i);
-      console.log("Value:", v);
-      console.log("Proof:", proof);
-    }
-  }
+// It first fetches the user data from the CSV file, then constructs a Merkle tree using the user data.
+async function getUserDataFromCSV(): Promise<{ address: string, amount: string }[]> {
+  return new Promise((resolve, reject) => {
+    let userData: { address: string, amount: string }[] = [];
+
+    fs.createReadStream('airdrop.csv')
+      .pipe(csvParser())
+      .on('data', (row: { address: string; amount: string }) => { 
+        userData.push({ address: row.address, amount: row.amount });
+      })
+      .on('end', () => {
+        resolve(userData);
+      })
+      .on('error', reject);
+  });
 }
 
-// // Run this to generate the Merkle tree Hash
-// generateMerkleTree();
 
-// //Run this to generate Proof for based on the selected address.
-// await generateProof();
-
-(async () => {
-  // Run this to generate the Merkle tree Hash
-  await generateMerkleTree();
-
-  // Run this to generate Proof based on the selected address.
-  await generateProof();
-})();
+export  { generateRootHash, generateMerkleProof, };
--- test/BaycGatedContract.ts
+++ test/BaycGatedContract.ts
@@ -1,77 +1,37 @@
 import { ethers } from "hardhat";
 import { expect } from "chai";
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { BaycGatedMerkleAirdrop, SYE } from "../typechain-types";
 import { MerkleTree } from "merkletreejs";
+// import { SignerWithAddress } from "@nomicfoundation/hardhat-toolbox/signers";
 import keccak256 from "keccak256";
-import { MockERC721 } from "@openzeppelin/contracts/mocks/MockERC721.sol";
+// import { MockERC721 } from "../typechain-types/MockERC721";
+import { generateRootHash } from "../merkle";
 
-describe("BaycGatedMerkleAirdrop", function () {
-  let baycGatedMerkleAirdrop: BaycGatedMerkleAirdrop;
-  let token: SYE;
-  let baycNft: MockERC721;
-  let owner: SignerWithAddress;
-  let user1: SignerWithAddress;
-  let user2: SignerWithAddress;
-  let merkleTree: MerkleTree;
+describe("BaycGatedContract", function () {
+    let baycGatedMerkleAirdrop: BaycGatedMerkleAirdrop;
+    let token: SYE;
+    let merkleTree: MerkleTree;
 
-  before(async function () {
-    [owner, user1, user2] = await ethers.getSigners();
+    async function deployToken() {
+        const SYE = await ethers.getContractFactory("SYE");
+        token = await SYE.deploy();
+       return token;
+    }
+    async function deployNFT() {
+        const token = await deployToken();
+        const baycAddress = "0xbB05F71952B30786d0aC7c7A8fA045724B8d2D69";
+        const rootHash = await generateRootHash();
+        const MockBAYC = await ethers.getContractFactory("BaycGatedMerkleAirdrop");
+        baycGatedMerkleAirdrop = await MockBAYC.deploy(token, baycAddress, rootHash);
+        return {baycGatedMerkleAirdrop, token, baycAddress, rootHash};
+    }
 
-    // Deploy SYE token
-    const SYEToken = await ethers.getContractFactory("SYE");
-    token = await SYEToken.deploy();
+    it("should deploy contracts", async function () {
+        const {baycGatedMerkleAirdrop, token, baycAddress, rootHash} = await deployNFT();
+        expect(baycGatedMerkleAirdrop).to.exist;
+        expect(token).to.exist;
+        expect(baycAddress).to.exist;
+        expect(rootHash).to.exist;
+    });
 
-    // Deploy mock BAYC NFT using OpenZeppelin's MockERC721
-    const MockBAYC = await ethers.getContractFactory("MockERC721");
-    baycNft = await MockBAYC.deploy("Bored Ape Yacht Club", "BAYC");
-
-    // Create merkle tree
-    const leaves = [
-      { address: user1.address, amount: ethers.utils.parseEther("100") },
-      { address: user2.address, amount: ethers.utils.parseEther("200") },
-    ].map((x) => ethers.utils.solidityKeccak256(["address", "uint256"], [x.address, x.amount]));
-
-    merkleTree = new MerkleTree(leaves, keccak256, { sortPairs: true });
-    const merkleRoot = merkleTree.getHexRoot();
-
-    // Deploy BaycGatedMerkleAirdrop contract
-    const BaycGatedMerkleAirdrop = await ethers.getContractFactory("BaycGatedMerkleAirdrop");
-    baycGatedMerkleAirdrop = await BaycGatedMerkleAirdrop.deploy(token.address, baycNft.address, merkleRoot);
-
-    // Mint tokens to the airdrop contract
-    await token.connect(owner).mint(ethers.utils.parseEther("1000"));
-    await token.connect(owner).transfer(baycGatedMerkleAirdrop.address, ethers.utils.parseEther("1000"));
-
-    // Mint BAYC NFT to user1
-    await baycNft.mint(user1.address, 1);
-  });
-
-  it("should allow a user who owns BAYC and has valid Merkle proof to claim airdrop", async function () {
-    const leaf = ethers.utils.solidityKeccak256(["address", "uint256"], [user1.address, ethers.utils.parseEther("100")]);
-    const proof = merkleTree.getHexProof(leaf);
-
-    await expect(baycGatedMerkleAirdrop.connect(user1).claimAirdrop(ethers.utils.parseEther("100"), proof))
-      .to.emit(baycGatedMerkleAirdrop, "AirdropClaimed")
-      .withArgs(user1.address, ethers.utils.parseEther("100"));
-
-    expect(await token.balanceOf(user1.address)).to.equal(ethers.utils.parseEther("100"));
-  });
-
-  it("should not allow a user without BAYC to claim airdrop", async function () {
-    const leaf = ethers.utils.solidityKeccak256(["address", "uint256"], [user2.address, ethers.utils.parseEther("200")]);
-    const proof = merkleTree.getHexProof(leaf);
-
-    await expect(baycGatedMerkleAirdrop.connect(user2).claimAirdrop(ethers.utils.parseEther("200"), proof))
-      .to.be.revertedWith("Must own a BAYC NFT to claim");
-  });
-
-  it("should not allow a user to claim twice", async function () {
-    const leaf = ethers.utils.solidityKeccak256(["address", "uint256"], [user1.address, ethers.utils.parseEther("100")]);
-    const proof = merkleTree.getHexProof(leaf);
-
-    await expect(baycGatedMerkleAirdrop.connect(user1).claimAirdrop(ethers.utils.parseEther("100"), proof))
-      .to.be.revertedWith("Airdrop already claimed.");
-  });
-
-});
+})
